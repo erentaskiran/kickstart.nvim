92333cf766ee9f461e91511c054af887c549feab
Eren Taşkıran <erentaskiran@Erens-MacBook-Pro.local>
2025-01-25 22:18:31 +0300

    neo tree config

--- init.lua
+++ init.lua
@@ -900,7 +900,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+    -- [[ Configure Treesitter ]] See :help nvim-treesitter
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
@@ -917,12 +917,10 @@ require('lazy').setup({
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
     --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+    --    - Incremental selection: Included, see :help nvim-treesitter-incremental-selection-mod
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-  },
-
-  -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
+  }, -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
 
@@ -935,8 +933,35 @@ require('lazy').setup({
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- Simgeler için
+      'MunifTanjim/nui.nvim',
+    },
+    config = function()
+      require('neo-tree').setup {
+        close_if_last_window = true, -- Son pencere ise otomatik kapat
+        popup_border_style = 'rounded',
+        enable_git_status = true, -- Git durumunu göster
+        enable_diagnostics = true, -- Tanılama bilgileri
+        filesystem = {
+          follow_current_file = true, -- Mevcut dosyayı takip et
+          hijack_netrw_behavior = 'open_current', -- Netrw'yu devre dışı bırak
+          use_libuv_file_watcher = true, -- Dosya değişikliklerini izleme
+        },
+      }
+
+      -- Keymaps
+      vim.keymap.set('n', '<leader>e', '<cmd>Neotree toggle<CR>', { desc = "Neo-tree'yi Aç/Kapat" })
+      vim.keymap.set('n', '<leader>f', '<cmd>Neotree focus<CR>', { desc = "Neo-tree'ye Odaklan" })
+      vim.keymap.set('n', '<leader>r', '<cmd>Neotree reveal<CR>', { desc = "Neo-tree'de Mevcut Dosyayı Göster" })
+      vim.keymap.set('n', '<leader>c', '<cmd>Neotree close<CR>', { desc = "Neo-tree'yi Kapat" })
+      vim.keymap.set('n', '<leader>n', '<cmd>Neotree show<CR>', { desc = "Neo-tree'yi Aç" })
+    end,
+  }, -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
